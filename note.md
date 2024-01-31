5928784479f96f3aeb9cce33424fe1365f22b84a
kieupie <92916187+kieupie@users.noreply.github.com>
2024-01-31 12:16:45 +0700

    impliment throttle

--- note.md
+++ note.md
@@ -5,3 +5,13 @@ Khi tìm kiếm với từ khóa là "Ben", Network tab:
 
 => Cần sử dụng kỹ thuật Debounce 
 # sử dụng setTimeout
+
+
+
+## Kỹ thuật Throttle
+Tương tự như debounce đảm bảo trong cùng một khoảng thời gian quy định, thì function chỉ thực thi một lần duy nhất.
+TH thường sử dụng: scroll, read text editor (gõ văn bản - autosave)
+
+Thời điểm callback được thực thi khác so với debounce 
+- Với debounce: Nếu set delay time 1000ms~1s, sau 1s mà user không nhập search bar nữa thì mới thực thi event handler.
+- Với Throttle: Nếu set wait time 1000ms~1s, thực thi ngay lập tức, rồi sau đó cứ 1s trôi qua nó lại thực thi lại event handler.
--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const resultBox = document.getElementById('result-box');
 
 let timeOut = null;
 
+let timeWait = false;
+let lastArgs = null;
+
 const debounce = (callback, delay) => {
     let timeOut = null;
 
@@ -14,7 +17,8 @@ const debounce = (callback, delay) => {
     }
 }
 
-const handleSearchChange = (e) => {
+//Debounce
+const handleSearchChange1 = (e) => {
     const searchTerm = e.target.value.trim() || '';
     console.log('searchTerm ' + searchTerm);
 
@@ -51,6 +55,63 @@ const handleSearchChange = (e) => {
         });
 }
 
+//Throttle
+const handleSearchChange2 = (e) => {
+    if (timeWait) {
+        //Trigger first event handlers
+        lastArgs = e;
+        return;
+    }
+
+    const searchTerm = e.target.value.trim() || '';
+    console.log('searchTerm ' + searchTerm);
+
+    if (searchTerm === '') {
+        resultBox.innerHTML = '';
+        resultBox.style.display = 'none';
+        return;
+    }
+
+    fetch(
+        'https://dummyjson.com/users/search?' +
+        new URLSearchParams({
+            q: searchTerm,
+        })
+    )
+        .then((res) => res.json())
+        .then((res) => {
+            document.getElementById('result-box').innerHTML = '';
+            if (res?.users?.length) {
+                res.users.forEach((user) => {
+                    const textElm = document.createElement('p');
+                    const userName = document.createTextNode(`${user.firstName} ${user.lastName}`);
+
+                    textElm.appendChild(userName);
+                    document.getElementById('result-box').appendChild(textElm);
+                });
+                resultBox.style.display = 'block';
+            } else {
+                resultBox.style.display = 'none';
+            }
+        })
+        .catch((err) => {
+            console.error("Error: " + err);
+        });
+
+    timeWait = true;
+
+    setTimeout(() => {
+        if (lastArgs === null) {
+            timeWait = false;
+        } else {
+            timeWait = false;
+            handleSearchChange2(lastArgs)
+            lastArgs = null;
+        }
+    }, 1000)
+}
+
 
 document.getElementById('search-field')
-    .addEventListener('input', debounce(handleSearchChange, 1000));
+    // .addEventListener('input', debounce(handleSearchChange1, 1000));
+    .addEventListener('input', handleSearchChange2);
