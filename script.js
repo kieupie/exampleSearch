127b124dd3d0bf22f344687e9afd463d0bb0ee6f
kieupie <92916187+kieupie@users.noreply.github.com>
2024-01-31 11:54:56 +0700

    reuse the debounce function.

--- script.js
+++ script.js
@@ -3,50 +3,54 @@ const resultBox = document.getElementById('result-box');
 
 let timeOut = null;
 
-document.getElementById('search-field').addEventListener('input', (e) => {
+const debounce = (callback, delay) => {
+    let timeOut = null;
 
-    clearTimeout(timeOut);
+    return (...args) => {
+        clearTimeout(timeOut);
+        timeOut = setTimeout(() => {
+            callback(...args);
+        }, delay);
+    }
+}
 
-    timeOut = setTimeout(() => {
+const handleSearchChange = (e) => {
+    const searchTerm = e.target.value.trim() || '';
+    console.log('searchTerm ' + searchTerm);
 
-        const searchTerm = e.target.value.trim() || '';
-        console.log('searchTerm ' + searchTerm);
+    if (searchTerm === '') {
+        resultBox.innerHTML = '';
+        resultBox.style.display = 'none';
+        return;
+    }
 
-        if (searchTerm === '') {
-            resultBox.innerHTML = '';
-            resultBox.style.display = 'none';
-            return;
-        }
+    fetch(
+        'https://dummyjson.com/users/search?' +
+        new URLSearchParams({
+            q: searchTerm,
+        })
+    )
+        .then((res) => res.json())
+        .then((res) => {
+            document.getElementById('result-box').innerHTML = '';
+            if (res?.users?.length) {
+                res.users.forEach((user) => {
+                    const textElm = document.createElement('p');
+                    const userName = document.createTextNode(`${user.firstName} ${user.lastName}`);
 
-        fetch(
-            'https://dummyjson.com/users/search?' +
-            new URLSearchParams({
-                q: searchTerm,
-            })
-        )
-            .then((res) => res.json())
-            .then((res) => {
-                document.getElementById('result-box').innerHTML = '';
-                if (res?.users?.length) {
-                    res.users.forEach((user) => {
-                        const textElm = document.createElement('p');
-                        const userName = document.createTextNode(`${user.firstName} ${user.lastName}`);
-
-                        textElm.appendChild(userName);
-                        document.getElementById('result-box').appendChild(textElm);
-                    });
-                    resultBox.style.display = 'block';
-                } else {
-                    resultBox.style.display = 'none';
-                }
-            })
-            .catch((err) => {
-                console.error("Error: " + err);
-            });
-
-    }, 1000)
+                    textElm.appendChild(userName);
+                    document.getElementById('result-box').appendChild(textElm);
+                });
+                resultBox.style.display = 'block';
+            } else {
+                resultBox.style.display = 'none';
+            }
+        })
+        .catch((err) => {
+            console.error("Error: " + err);
+        });
+}
 
 
-
-
-});
+document.getElementById('search-field')
+    .addEventListener('input', debounce(handleSearchChange, 1000));
